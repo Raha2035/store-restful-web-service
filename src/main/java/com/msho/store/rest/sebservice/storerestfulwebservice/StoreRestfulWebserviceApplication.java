e1368b03b5797936ccf4bf9274568b7d72a73b49
Msho <msheikhoveisi@gmail.com>
2024-06-08 13:24:12 +0330

    01-Initial commit for the project. This sets up the basic structure.

---
+++ src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/StoreRestfulWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.msho.store.rest.sebservice.storerestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRestfulWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreRestfulWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msho/store/rest/sebservice/storerestfulwebservice/StoreRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msho.store.rest.sebservice.storerestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreRestfulWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
