e623eb1087be262c6c31505380b458175e155522
Msho <msheikhoveisi@gmail.com>
2024-06-12 14:06:51 +0330

    10-Add exception handling to project.

---
+++ src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/exception/ErrorDetails.java
@@ -0,0 +1,30 @@
+package com.msho.store.rest.sebservice.storerestfulwebservice.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+
+    private LocalDateTime timeStamp;
+
+    private String message;
+
+    private String details;
+
+    public ErrorDetails(LocalDateTime timeStamp, String message, String details) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
