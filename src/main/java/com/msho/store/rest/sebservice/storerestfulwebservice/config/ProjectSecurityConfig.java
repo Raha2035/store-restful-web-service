66f290e9b9dfb0eb6abfe903e2088edb8bd01530
Msho <msheikhoveisi@gmail.com>
2024-06-18 17:59:21 +0330

    15-JWT Spring Security

--- src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/config/ProjectSecurityConfig.java
+++ src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/config/ProjectSecurityConfig.java
@@ -1,36 +1,62 @@
 package com.msho.store.rest.sebservice.storerestfulwebservice.config;
 
-import com.msho.store.rest.sebservice.storerestfulwebservice.filter.CsrfCookieFilter;
+import com.msho.store.rest.sebservice.storerestfulwebservice.filter.JwtAuthenticationFilter;
+import com.msho.store.rest.sebservice.storerestfulwebservice.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class ProjectSecurityConfig {
 
+    private final UserService userService;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final CustomLogoutHandler logoutHandler;
+
+    public ProjectSecurityConfig(UserService userService, JwtAuthenticationFilter jwtAuthenticationFilter, CustomLogoutHandler logoutHandler) {
+        this.userService = userService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.logoutHandler = logoutHandler;
+    }
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        requestHandler.setCsrfRequestAttributeName("_csrf");
-        http.csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/users/create-user")
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                .authorizeHttpRequests((requests) -> {
-//            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
-            requests.requestMatchers("/users/create-user").permitAll()
-                    /*.requestMatchers("/users/all-users").authenticated()*/
-                    .anyRequest().authenticated();
-        });
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain)http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req -> req.requestMatchers("/login/**", "/register/**", "/refresh_token/**")
+                                .permitAll()
+                                /*.requestMatchers("/admin_only/**").hasAuthority("ADMIN")*/
+                                .anyRequest()
+                                .authenticated()
+                ).userDetailsService(userService)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(
+                        e -> e.accessDeniedHandler(
+                                        (request, response, accessDeniedException) -> response.setStatus(403)
+                                )
+                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .logout(l -> l
+                        .logoutUrl("/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()
+                        ))
+                .build();
     }
 
     @Bean
@@ -38,35 +64,15 @@ public class ProjectSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
 
-
-/*
-        "/users/all-users",
-        "/users/get-user/{id}",
-        "/users/delete-user/{id}",
-        "/users/{userId}/all-orders",
-        "/users/{userId}/create-order",
-        "/users/{userId}/delete-order/{id}",
-        "/users/{userId}/update-order/{id}",
-        "/categories/all-categories",
-        "/categories/get-category/{id}",
-        "/categories/create-category",
-        "/categories/delete-category/{id}",
-        "/categories/update-category/{id}",
-        "/categories/{categoryId}/products/create-product",
-        "/categories/{categoryId}/products/all-products",
-        "/categories/{categoryId}/products/get-product/{productId}",
-        "/categories/{categoryId}/products/update-product/{productId}",
-        "/categories/{categoryId}/products/delete-product/{productId}",
-        "/categories/{categoryId}/products/delete-all-products",
-        "/stores/all-stores",
-        "/stores/get-store/{id}",
-        "/stores/create-store",
-        "/stores/delete-store/{id}",
-        "/stores/update-store/{id}",
-        "/stores/{id}/all-products",
-        "/stores/{id}/create-products",
-        "/stores/{storeId}/products/delete-product/{productId}",
-        "/stores/{storeId}/products/delete-all-products")
-*/
+/*http.authorizeHttpRequests(request -> request.requestMatchers("/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html" , "/users/**", "/categories/**").permitAll()
+                .anyRequest().authenticated());
+        return http.build();*/
---
+++ src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/service/ProductService.java
@@ -0,0 +1,2 @@
+package com.msho.store.rest.sebservice.storerestfulwebservice.service;public class ProductService {
+}
