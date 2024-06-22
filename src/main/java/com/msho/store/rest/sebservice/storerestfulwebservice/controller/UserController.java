14612f50ce393e134ee782fdb36e597a708ab6d0
Msho <msheikhoveisi@gmail.com>
2024-06-22 13:12:51 +0330

    17-Refactor: Improve UserController readability and consistency

--- src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/controller/UserController.java
+++ src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/controller/UserController.java
@@ -8,60 +8,61 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 public class UserController {
     private final UserService userService;
-
     private final OrdersService ordersService;
 
     public UserController(UserService userService, OrdersService ordersService) {
-
         this.userService = userService;
         this.ordersService = ordersService;
     }
 
     @GetMapping("/users/all-users")
-    public ResponseEntity<Object> findAllUsers(){
-        return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(userService.allUsers());
     }
 
     @GetMapping("/users/get-user/{id}")
-    public ResponseEntity<User> findOneUser(@NotNull @PathVariable int id){
-        return new ResponseEntity<>(
-                userService.getSpecificUser(id)
-                , HttpStatus.OK);
+    public ResponseEntity<User> getUserById(@NotNull @PathVariable int id){
+        return ResponseEntity.ok(userService.getSpecificUser(id));
     }
 
     @DeleteMapping("/users/delete-user/{id}")
     public ResponseEntity<String> deleteUser(@NotNull @PathVariable int id){
        userService.deleteSpecificUser(id);
-       return new ResponseEntity<>("user successfully deleted.", HttpStatus.OK);
+       return ResponseEntity.ok("user successfully deleted.");
     }
 
     @GetMapping("/users/{userId}/all-orders")
-    public ResponseEntity<Object> findAllOrdersOfOneUser(@PathVariable int userId){
+    public ResponseEntity<List<Orders>> getAllOrdersByUserId(@PathVariable int userId){
         List<Orders> orders = ordersService.findAllOrdersOfOneUser(userId);
-        return new ResponseEntity<>(orders, HttpStatus.OK);
+        return ResponseEntity.ok(orders);
     }
 
     @PostMapping("/users/{userId}/create-order")
-    public ResponseEntity<Object> createOrderForOneUser(@PathVariable int userId, @RequestBody Orders order){
+    public ResponseEntity<String> createOrder(@PathVariable int userId,
+                                              @RequestBody Orders order){
         ordersService.createOrderForOneUser(userId ,order);
-        return new ResponseEntity<>("This order successfully saved", HttpStatus.CREATED);
+        return ResponseEntity.status( HttpStatus.CREATED).body("This order successfully saved");
     }
 
     @DeleteMapping("/users/{userId}/delete-order/{id}")
-    public ResponseEntity<Object> deleteOneOrderById(@PathVariable int userId, @PathVariable int id){
+    public ResponseEntity<Object> deleteOrder(@PathVariable int userId,
+                                              @PathVariable int id){
         ordersService.deleteOrderById(userId, id);
-        return new ResponseEntity<>("Order successfully deleted", HttpStatus.OK);
+        return ResponseEntity.ok("Order successfully deleted");
     }
 
     @PutMapping("/users/{userId}/update-order/{id}")
-    public ResponseEntity<Object> modifyOrder(@PathVariable int userId, @PathVariable int id, @Valid @RequestBody Orders order){
+    public ResponseEntity<String> updateOrder(@PathVariable int userId,
+                                              @PathVariable int id,
+                                              @Valid @RequestBody Orders order){
         ordersService.modifyOrder(userId, id, order);
-        return new ResponseEntity<>("Order successfully updated", HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Order successfully updated");
     }
 }
 
