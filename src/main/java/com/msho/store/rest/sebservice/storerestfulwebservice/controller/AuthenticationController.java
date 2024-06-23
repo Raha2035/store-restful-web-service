a8a5dcc10012f1beef88a34c07b9a10c9cef4cd7
Msho <msheikhoveisi@gmail.com>
2024-06-23 08:40:47 +0330

    22-Refactor: Improve AuthenticationController readability and consistency

--- src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/controller/AuthenticationController.java
+++ src/main/java/com/msho/store/rest/sebservice/storerestfulwebservice/controller/AuthenticationController.java
@@ -11,23 +11,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+* REST controller for managing user authentication.
+* */
 @RestController
 public class AuthenticationController {
 
     private final AuthenticationService authService;
 
+    /*
+    * Constructor for AuthenticationController.
+    *
+    * @param authService the authentication service
+    * */
     public AuthenticationController(AuthenticationService authService) {
         this.authService = authService;
     }
 
 
+    /*
+    * Register a new user.
+    *
+    * @param request the user registration request
+    * @return the authentication response
+    * */
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody User request
             ) {
-        return new ResponseEntity<>(authService.register(request), HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(request));
     }
 
+    /*
+    * Authenticate a user.
+    *
+    * @param request the user login request
+    * @return the authentication response
+    * */
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(
             @RequestBody User request
@@ -35,6 +55,13 @@ public class AuthenticationController {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
+    /*
+    * Refresh the authentication token.
+    *
+    * @param request the HTTP servlet request
+    * @param response the HTTP servlet response
+    * @return the
+    * */
     @PostMapping("/refresh_token")
     public ResponseEntity<Object> refreshToken(
             HttpServletRequest request,
